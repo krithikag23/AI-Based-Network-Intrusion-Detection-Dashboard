f904883dc1f7958d4b59efd95870aa51950c2390
Krithika Ganesan <113279833+krithikag23@users.noreply.github.com>
2026-03-21 10:47:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from transformers import pipeline
 
+# Load summarizer model
+summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+
+st.title("📄 Smart Research Paper Summarizer")
+
+uploaded_file = st.file_uploader("Upload a PDF", type="pdf")
