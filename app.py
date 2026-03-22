0e6a94a1499d8c3e5218417744c285f51d904460
Krithika Ganesan <113279833+krithikag23@users.noreply.github.com>
2026-03-22 14:18:02 +0530

    Add input fields for network traffic data

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ from model import train_model
 st.title("🔐 AI Intrusion Detection System")
 
 model = train_model()
+
+
+st.subheader("Enter Network Traffic Data")
+
+duration = st.number_input("Duration", value=1)
+src_bytes = st.number_input("Source Bytes", value=100)
+dst_bytes = st.number_input("Destination Bytes", value=200)
