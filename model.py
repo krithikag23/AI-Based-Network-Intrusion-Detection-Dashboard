6fba9d4bfac4d8930a24033f292f18c59d3116a2
Krithika Ganesan <113279833+krithikag23@users.noreply.github.com>
2026-03-24 14:13:50 +0530

    Implement model training function
    Added a function to train a Random Forest model using data from a CSV file.

--- model.py
+++ model.py
@@ -2,3 +2,13 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
+def train_model():
+    df = pd.read_csv("data/sample_data.csv")
+    
+    X = df.drop("label", axis=1)
+    y = df["label"]
+    
+    model = RandomForestClassifier()
+    model.fit(X, y)
+    
+    return model
