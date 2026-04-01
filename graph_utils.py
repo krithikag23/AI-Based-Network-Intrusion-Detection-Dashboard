423d83a82d3f9fd5a57d765255b5b0aa9323ae4c
Krithika Ganesan <113279833+krithikag23@users.noreply.github.com>
2026-04-01 18:33:38 +0530

    Update graph_utils.py

--- graph_utils.py
+++ graph_utils.py
@@ -1,1 +1,15 @@
 import networkx as nx
+def create_graph():
+    G = nx.Graph()
+
+    # Add nodes (devices/IPs)
+    G.add_node("A")
+    G.add_node("B")
+    G.add_node("C")
+
+    # Add edges (connections)
+    G.add_edge("A", "B", weight=100)
+    G.add_edge("B", "C", weight=5000)  # suspicious
+    G.add_edge("A", "C", weight=200)
+
+    return G
